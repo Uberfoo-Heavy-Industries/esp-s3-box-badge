e6978c817725a5f675057baba04f21d9d6a3ddc6
James Bryant <13969279+james-bryant@users.noreply.github.com>
2024-07-14 22:17:15 -0500

    make pages take full screen

--- main/ui/Page.cpp
+++ main/ui/Page.cpp
@@ -1,7 +1,9 @@
+#include "bsp/esp-bsp.h"
 #include "Page.h"
 
 Page::Page(lv_obj_t *parent) {
     page = lv_obj_create(parent);
+    lv_obj_set_size(page, BSP_LCD_H_RES, LV_VER_RES); // Set the size of the page to fill the screen
     lv_obj_add_flag(page, LV_OBJ_FLAG_HIDDEN);
 }
 
